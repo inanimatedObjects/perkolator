a743ae9a1b7c2c763c10f61d8abab28f75d02d1f
Caleb Kress <calebkress@gmail.com>
2018-07-24 11:00:37 -0500

    imported Perkolator-caleb from CompCompCalc

---
+++ Perkolator-caleb/data.js
@@ -0,0 +1,14 @@
+var data = [
+  { name: "cash1", cardTitle: "Signing Bonus", value: 5000, max: 10000 },
+  { name: "cash2", cardTitle: "Base Salary", value: 100000, max: 120000 },
+  { name: "cash3", cardTitle: "Incentive Comp - Cash", value: 10000, max: 15000 },
+  { name: "equity1", cardTitle: "Signing Equity", value: 20000, max: 31000 },
+  { name: "equity2", cardTitle: "eqRate", value: 5000, max: 14000 },
+  { name: "equity3", cardTitle: "Incentive Comp - Equity", value: 2000, max: 10000 },
+  { name: "perks1", cardTitle: "Stipends", value: 5000, max: 10000 },
+  { name: "perks2", cardTitle: "Benefits", value: 30000, max: 50000 },
+  { name: "perks3", cardTitle: "Retirement Savings", value: 1000, max: 2200 },
+  { name: "time1", cardTitle: "timeRate", value: 500, max: 1000 },
+  { name: "time2", cardTitle: "timeAllocation", value: 200, max: 1000 },
+  { name: "time3", cardTitle: "timeUtilization", value: 700, max: 1000 }
+];
