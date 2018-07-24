05663efd9c86f1d79597c2147dd227b23a7e2537
Noble Millie <noble.millie@gmail.com>
2018-07-23 22:11:18 -0500

    app architecture w/ @calebkress

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,6 @@
 window.d3 = undefined;
 
-import OfferSummary from '_viz_dashboard_OfferSummary';
-import StackedBar from '/_viz_dashboard_StackedBar';
-import SliderPanel from './_viz_dashboard_SliderPanel';
-import GraphicPanel from './_viz_dashboard_GraphicPanel';
-import RadarChart from './_viz_dashboard_component_RadarChart';
-
-// new RadarChart('radar');
+import SummaryOutput from '_OfferSummary';
+import StackedBar from '/_StackedBar';
+import SliderPanel from './_SliderPanel';
+import AnalyticPanel from './_Analytic';
