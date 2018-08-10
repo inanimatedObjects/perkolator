a2d8fdbad07bb7ed2b8b8d1461832779e7c0553d
Noble Millie <noble.millie@gmail.com>
2018-08-10 13:26:21 -0700

    collapsible settings panel for range endpoints added to index

---
+++ d3/src/d3.js
@@ -0,0 +1,61 @@
+
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+    svg.height = 300;
+    svg.width = 100;
+
+var panel = svg.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .attr("fill", "lightgrey")
+    .attr("stroke-width", 15)
+    .attr("stroke-linejoin", "round")
+    .style("opacity", 0.8)
+    .text("Panel");
+  // .selectAll("panel")
+  //   .data(["summaryPanel", "stackedBarPanel", "sliderPanel", "analyticsPanel"])
+  //   .enter().append("panel").append("text")
+  //   .text("Panel");
+
+svg.selectAll(".panel").append("g").append("text")
+  .attr("class", "panel")
+  .attr("transform", "translate(10, 20)")
+  .text("summaryPanel");
+
+
+
+//
+// var svg = d3.select("#perkolator"),
+//     width = +svg.attr("width"),
+//     height = +svg.attr("height"),
+//     angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
+//
+// svg.attr("transform", "translate(" + 0 + "," + -650 + ")");
+//
+var path = svg.append("g")
+    .attr("transform", "translate(" + width / 2 + 380 + "," + height / 2 + 50 + ")")
+    .attr("fill", "none")
+    .attr("stroke-width", 15)
+    .attr("stroke-linejoin", "round")
+    .style("opacity", 0.3)
+  .selectAll("path")
+  .data(["cyan", "magenta", "yellow"])
+  .enter().append("path")
+    .attr("stroke", function(d) { return d; })
+    .style("mix-blend-mode", "darken")
+    .datum(function(d, i) {
+      return d3.radialLine()
+          .curve(d3.curveLinearClosed)
+          .angle(function(a) { return a; })
+          .radius(function(a) {
+            var t = d3.now() / 3000;
+            return 700 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * 32;
+          });
+    });
+//
+// d3.timer(function() {
+//   path.attr("d", function(d) {
+//     return d(angles);
+//   });
+// });
